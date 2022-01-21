0966b335f3acf3c3642355f418bc43a33f8cb8a4
ltxlong <416803647@qq.com>
2022-01-21 12:14:35 +0800

    自定义全局异常

--- app/common/base/BaseException.php
+++ app/common/base/BaseException.php
@@ -10,24 +10,24 @@ use think\Exception;
 class BaseException extends Exception
 {
     // 状态码
-    public $code = 400;
+    public $status = 400;
     // 错误信息
-    public $message = 'error';
+    public $msg = 'error';
     // 错误码
-    public $errorCode = 400;
+    public $code = 400;
     // 附加信息
     public $data = [];
 
     public function __construct($params = [])
     {
-        if (array_key_exists('code', $params)) {
-            $this->code = $params['code'];
+        if (array_key_exists('status', $params)) {
+            $this->status = $params['status'];
         }
         if (array_key_exists('msg', $params)) {
-            $this->message = $params['msg'];
+            $this->msg = $params['msg'];
         }
-        if (array_key_exists('errorCode', $params)) {
-            $this->errorCode = $params['errorCode'];
+        if (array_key_exists('code', $params)) {
+            $this->code = $params['code'];
         }
         if (array_key_exists('data', $params)) {
             $this->data = $params['data'];
